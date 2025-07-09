7e02543417243f09dd9adb9764bdd0fea8a4ad66
ohcuy <everywavyday@gmail.com>
2025-07-09 15:14:27 +0900

    feat: greenWaves 추가 및 WatermelonView 수정
    - WatermelonBackgroundView를 WatermelonView로 합쳐서 패딩값을 자동으로 조정하기 쉽게 변경

--- tongtongtong/tongtongtong/Components/WatermelonView.swift
+++ tongtongtong/tongtongtong/Components/WatermelonView.swift
@@ -9,26 +9,44 @@ import SwiftUI
 
 struct WatermelonView: View {
     let isMicActive: Bool
+    let isRedBackground: Bool
     let onTap: () -> Void
     
     var body: some View {
         ZStack {
-            Image("WholeWatermelon")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: UIConstants.watermelonSize, height: UIConstants.watermelonSize)
-                .offset(y: -60)
-                .onTapGesture {
-                    if !isMicActive {
-                        onTap()
-                    }
-                }
+            let waveConfigs = isRedBackground ? WaveCircleConfig.redWaves : WaveCircleConfig.greenWaves
+            
+            ForEach(Array(waveConfigs.enumerated()), id: \.offset) { _, config in
+                WaveCircle(
+                    color: config.color.opacity(config.opacity),
+                    baseSize: config.baseSize,
+                    scale: config.scale,
+                    delay: config.delay,
+                    opacity: config.opacity
+                )
+            }
+            
+            Circle()
+                .frame(width: 220, height: 220)
+                .foregroundStyle(Color.white)
+                .overlay(
+                    Image("WholeWatermelon")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: UIConstants.watermelonSize, height: UIConstants.watermelonSize)
+                        .onTapGesture {
+                            if !isMicActive {
+                                onTap()
+                            }
+                        }
+                )
         }
     }
 }
 
 #Preview {
-    WatermelonView(isMicActive: false) {
+    WatermelonView(isMicActive: false, isRedBackground: false) {
         print("Watermelon tapped")
     }
-} 
+    .background(Color.blue)
+}
